82d42aa723d70d574d69c3ac4b1a08ea39ee3ffd
Isaac Stead <isaac_stead@eva.mpg.de>
2021-10-31 14:17:19 +0100

    Readme, source notes

--- bayes.lisp
+++ bayes.lisp
@@ -18,6 +18,17 @@
      (exp (* -1 n))
      (sqrt (* 2 pi n))))
 
+;;; "While most parameters are continuous, capable of taking on an
+;;; infinite number of values, it turns out that we can achieve an
+;;; excellent approximation of the continuous posterior distribution
+;;; by considering only a finite grid of parameter values. At any
+;;; particular value of a parameter, p′, it’s a simple matter to
+;;; compute the posterior probability: just multiply the prior
+;;; probability of p′ by the likelihood at p′ . Repeating this
+;;; procedure for each value in the grid generates an approximate
+;;; picture of the exact posterior distribution. This procedure is
+;;; called grid approximation."
+
 (defun spaced-seq (range-start range-end length)
   (let ((step (/ (- range-end range-start)
                  (1- length))))
@@ -37,9 +48,10 @@
     (mapcar (lambda (p) (/ p unstd-sum))
             unstd-posterior)))
 
-;;; Globe tossing example, translated from R:
-(defun globe-toss ()
-  (let ((posterior-grid (spaced-seq 0 1 20))
-        (flat-prior     (loop for i below 20 collect 1)))
+;;; Globe tossing example, translated from R. Produces identical result:
+(defun globe-toss (n-points)
+  (let ((posterior-grid (spaced-seq 0 1 n-points))
+        (flat-prior     (loop for i below n-points collect 1)))
     (grid-posterior (grid-likelihood 6 9 posterior-grid)
-                    prior)))
+                    flat-prior)))
+
